4ea2cda0e5336ab2e147fdebe8893699e03154fb
Manik <mggmanik@gmail.com>
2018-12-28 13:41:43 +0530

    added add/edit recipe button in the recipes page

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -1,25 +1,12 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
 
-/**
- * Generated class for the RecipePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-recipe',
   templateUrl: 'recipe.html',
 })
 export class RecipePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RecipePage');
+  constructor() {
   }
 
 }
